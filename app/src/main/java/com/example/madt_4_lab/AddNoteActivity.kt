d836a5f201fcc22e4c701acc00b9616826f3b408
gireyosmanov <girey345@gmail.com>
2023-11-12 21:31:48 +0200

    added AddNoteActivity and DeleteNoteActivity

--- app/src/main/java/com/example/madt_4_lab/AddNoteActivity.kt
+++ app/src/main/java/com/example/madt_4_lab/AddNoteActivity.kt
@@ -1,11 +1,11 @@
 package com.example.madt_4_lab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class AddNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        setContentView(R.layout.activity_main)
     }
 }
